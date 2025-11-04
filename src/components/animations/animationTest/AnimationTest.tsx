a78d670ce64503f594aef1ed42c6131c22a069fa
horameus <ludo.hartmann@gmail.com>
2025-11-04 18:46:30 +0100

    testing animations

---
+++ src/components/animations/animationTest/AnimationTest.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+import './animation.css';
+
+export function AnimationTest() {
+    return (
+        <Typography variant="h1" color="primary" className="animation">
+            HORAMEUS
+        </Typography>
+    );
+}
