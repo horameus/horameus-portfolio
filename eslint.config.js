011b9a215ba293b4bb9f796bc64216c51867ba9d
horameus <ludo.hartmann@gmail.com>
2025-11-04 15:38:45 +0100

    prettier and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { globalIgnores } from 'eslint/config'
 
-export default defineConfig([
+export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
@@ -14,6 +14,8 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      "eslint:recommended", 
+      "plugin:prettier/recommended"
     ],
     languageOptions: {
       ecmaVersion: 2020,
