4ccd860d1724de46f50f5725d1db6a60e7214f9c
horameus <ludo.hartmann@gmail.com>
2025-11-04 16:01:57 +0100

    theme init

---
+++ src/utils/theme/palette.ts
@@ -0,0 +1,13 @@
+import { PaletteOptions } from '@mui/material/styles';
+
+export const palette: Partial<PaletteOptions> = {
+    background: {
+        paper: '#FFFFFF',
+    },
+    text: {
+        primary: '#FFFFFF',
+        secondary: '#000000',
+    },
+    primary: { main: '#8efa46' },
+    secondary: { main: '#9938fc' },
+};
