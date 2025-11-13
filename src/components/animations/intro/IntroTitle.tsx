0a83e0143529e582048d016b70bce694a20e3b16
horameus <ludo.hartmann@gmail.com>
2025-11-13 19:01:53 +0100

    some animation tests

--- src/components/animations/intro/IntroTitle.tsx
+++ src/components/animations/intro/IntroTitle.tsx
@@ -3,10 +3,10 @@ import './styles.css';
 
 export function IntroTitle() {
     return (
-        <div className="animationContainer">
-            <h1 className="introTitle animation1">WELCOME TO</h1>
-            <h1 className="introTitle animation2">PROJECT</h1>
-            <h1 className="introTitle animation3 primary">HORAMEUS</h1>
+        <div className="animation-container">
+            <h1 className="intro-text animation1">WELCOME TO</h1>
+            <h1 className="intro-text animation2">PROJECT</h1>
+            <h1 className="intro-title animation3 primary">HORAMEUS</h1>
         </div>
     );
 }
